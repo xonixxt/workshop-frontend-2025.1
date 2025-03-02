07477b0dfaa3426b37b2c33842223c2541edcf66
xonixxt <jota.bbiel@gmail.com>
2025-03-02 09:53:18 -0300

    poketcg v

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 async function buscar() {
-var nomeCarta = document.getElementById('pesquisa').value;
-var apiKey = '1e9da96c-6f8e-4c91-8706-f9764654cdb0';
-var url = 'https://api.pokemontcg.io/v2/cards?q=name:' + nomeCarta + apiKey;
-var resposta = fetch(url);
-var dados = await resposta.json();
-var resultado = document.getElementById('resultado');
-resultado.innerHTML = '';
-
-if (dados.data.length === 0) {
-resultado.innerHTML = 'Nenhuma carta encontrada.';
-} else {
-dados.data.forEach(function(carta) {
-var cartaDiv = document.createElement('div');
-
-cartaDiv.innerHTML = '<h2>' + carta.name + '</h2><img src="' + carta.images.small + '"><p>Raridade: ' + carta.rarity + '</p>';
-resultado.appendChild(cartaDiv);
-});
-}
-}
+    nomeCarta = document.getElementById('pesquisa').value.toLowerCase();
+    apiKey = '1e9da96c-6f8e-4c91-8706-f9764654cdb0'
+    url = `https://api.pokemontcg.io/v2/cards?q=name:${nomeCarta}`
+    resposta = await fetch(url)
+    dados = await resposta.json()
+    resultado = document.getElementById(`resultado`)
+    resultado.innerHTML = ``
+    
+    if (dados.data.length == 0) {   
+        let cartaDiv = document.createElement('div')
+        cartaDiv.innerHTML = `Nenhuma carta encontrada`
+        resultado.appendChild(cartaDiv)
+    } else {
+        let cartaDiv = document.createElement('div')
+        carta = dados.data[0]
+        
+        cartaDiv.innerHTML = `
+        <h2>${carta.name}</h2>
+        <img src="${carta.images.small}" />
+        <p>Raridade: ${carta.rarity}</p>
+        <hr>
+        `
+        resultado.appendChild(cartaDiv)
+    }
+    
+    }
+    
